d416d51df08ede1f4ecbea135172ae609ab91cb9
tseitzin <tseitzinger@gmail.com>
2025-11-19 20:18:03 -0500

    Add CSRF token to axios requests

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -6,6 +6,34 @@ import authService from './authService'
 
 axios.defaults.withCredentials = true
 
+/**
+ * Get CSRF token from cookie
+ */
+function getCsrfTokenFromCookie() {
+  const name = 'bible_qa_csrf='
+  const cookies = document.cookie.split(';')
+  for (let cookie of cookies) {
+    cookie = cookie.trim()
+    if (cookie.startsWith(name)) {
+      return cookie.substring(name.length)
+    }
+  }
+  return null
+}
+
+// Add CSRF token to requests
+axios.interceptors.request.use(
+  (config) => {
+    // Add CSRF token for state-changing requests
+    const csrfToken = getCsrfTokenFromCookie()
+    if (csrfToken && config.method && !['get', 'head', 'options'].includes(config.method.toLowerCase())) {
+      config.headers['X-CSRF-Token'] = csrfToken
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
 // Handle authentication errors globally
 axios.interceptors.response.use(
   (response) => response,
